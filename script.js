b61f9f7ae181fef6ab56afc4d9b8bfe367f395fc
Andrea <j.andrea.nelson@gmail.com>
2017-03-01 22:03:27 -0500

    do while loop added

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-  var userInput= prompt("How many squares would you like on each side?");
-// If x is Not a Number or less than one or greater than 10
-    if (isNaN(userInput) || userInput < 1 || userInput > 100) {
-     prompt("Please enter a number between 1 and 100");
- } else {
-   text = "valid";
-     }
-     document.getElementById("demo").innerHTML = text;
 
- });
+  var userInput;
+  var inputValid= false;
+  var i = 0;
+
+  do {
+     userInput= prompt("How many squares would you like on each side?");
+    if (isNaN(userInput) || userInput < 1 || userInput > 100) {
+      // If invalid entry
+     alert("Please enter a number between 1 and 100");
+
+ } else {
+   // valid number entered
+   inputValid = true;
+     }
+ } while(!inputValid);
