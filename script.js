3fca02a0a61497afb1fb7c8ffea18b7763267b2b
Andrea <j.andrea.nelson@gmail.com>
2017-03-01 22:26:28 -0500

    able to extract numerical input value

--- script.js
+++ script.js
@@ -4,13 +4,18 @@
   var i = 0;
 
   do {
-     userInput= prompt("How many squares would you like on each side?");
+     userInput= prompt("How many squares would you like on each side?","0");
     if (isNaN(userInput) || userInput < 1 || userInput > 100) {
       // If invalid entry
      alert("Please enter a number between 1 and 100");
 
+
  } else {
    // valid number entered
+
    inputValid = true;
      }
  } while(!inputValid);
+
+var box = parseInt(userInput);
+alert(box+1);
